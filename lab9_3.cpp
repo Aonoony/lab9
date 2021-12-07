1727dfa857634ded79d10ba6ab53c3f1b5071dd3
Aonoony <95071022+Aonoony@users.noreply.github.com>
2021-12-07 16:11:47 +0700

    "lab9_3.cpp completed"

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -9,17 +9,6 @@
 "ONE-NIGHT-STAND"
 "MARRIED"
 */
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your property: "
-"Your status = "
-"UNFRIEND"
-"FRIEND"
-"BEST FRIEND"
-"ONE-NIGHT-STAND"
-"MARRIED"
-*/
 #include<iostream>
 using namespace std;
 int main(){
